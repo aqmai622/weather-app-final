dcb5fcfa2becd989709699d8161bd0cb949c0409
aqmai622 <137582587+aqmai622@users.noreply.github.com>
2023-07-02 10:05:08 -0500

    changed API Url, stuck

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 let apiKey="8dfa2ecf69579ab13176cfeea25a2d6c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiKey);
 
