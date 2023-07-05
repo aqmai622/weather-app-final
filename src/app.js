6368bcce4098f402d9b27e4aa7046e83159e0e34
aqmai622 <137582587+aqmai622@users.noreply.github.com>
2023-07-04 20:11:01 -0500

    Added the search engine!

--- src/app.js
+++ src/app.js
@@ -43,8 +43,24 @@ iconElement.setAttribute (
 iconElement.setAttribute ("alt", response.data.weather[0].description);
 }
 
+function search(city){
 let apiKey = "8dfa2ecf69579ab13176cfeea25a2d6c";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSubmit (event) {
+    event.preventDefault ();
+   let cityInputElement = document.querySelector("#city-input");
+   search(cityInputElement.value)
+}
+
+
+search("New York");
+
+
+
+
+let form = document.querySelector("#searh-form");
+form.addEventListener("submit", handleSubmit);
